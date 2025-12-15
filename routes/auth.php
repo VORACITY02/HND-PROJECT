60cd1899df729ff648b265241d2f00dacc50ec0d
Voracity02 <raw04456@gmail.com>
2025-12-15 11:43:44 +0100

    second commit

--- routes/auth.php
+++ routes/auth.php
@@ -74,6 +74,6 @@ Route::middleware(['auth'])->group(function () {
     })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
